ed724e06716cfef2d90b295d1c20c9414f00612d
decpk <37063026+decpk@users.noreply.github.com>
2023-03-05 01:05:59 +0530

    File name font-weight increased

--- src/molecules/RenderFile/RenderFile.styles.ts
+++ src/molecules/RenderFile/RenderFile.styles.ts
@@ -1,21 +1,22 @@
 import styled from "styled-components";
 
 export const RenderFileWrapper = styled.button`
-  display         : flex;
-  flex-direction  : column;
-  align-items     : center;
-  justify-content : center;
-  white-space     : nowrap;
-  padding         : 0.5rem 1rem;
-  border-radius   : 4px;
-  font-size       : 0.85rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  font-size: 0.85rem;
   background-color: transparent;
-  border          : none;
-  opacity: ${(props) => props.name?.startsWith('.') ? '0.3': 1};
+  border: none;
+  opacity: ${(props) => (props.name?.startsWith(".") ? "0.3" : 1)};
 
-  img, svg {
+  img,
+  svg {
     height: 60px;
-    width : 60px;
+    width: 60px;
   }
 
   :hover {
@@ -24,9 +25,10 @@ export const RenderFileWrapper = styled.button`
 `;
 
 export const StyledFileName = styled.div`
-  width        : 70px;
+  width: 70px;
   text-overflow: ellipsis;
-  overflow     : hidden;
-  cursor       : default;
-  font-size    : 12px;
+  overflow: hidden;
+  cursor: default;
+  font-size: 12px;
+  font-weight: 600;
 `;
